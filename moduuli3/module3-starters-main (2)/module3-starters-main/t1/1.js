e73910d4f86f84715a5fb2cddd32f831c93cc39c
iidaaald <iida.aaldijk@metropolia.fi>
2024-11-19 11:09:36 +0200

    työn alla

---
+++ moduuli3/module3-starters-main (2)/module3-starters-main/t1/1.js
@@ -0,0 +1,23 @@
+/*
+function createLi(text) {
+  const targetElement = document.querySelector("#target")
+  const li = document.createElement("Li")
+  li.innerText = Text
+  targetElement.appendChild(li)
+}
+
+createLi("First item")
+
+const li1 = createLi("First item")
+li1.innerText = "First item"
+
+ */
+document.querySelector("target").innerHTML = `<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>`
+element.classList.add("my-list");
+
+function addClass() {
+    const element = document.getElementById("myElement");
+    element.classList.add("newClass");
+}
